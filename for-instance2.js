571c727e607a1e5f35753ed8c6a0f57c33694442
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-17 20:33:29 -0400

    ...

--- for-instance2.js
+++ for-instance2.js
@@ -1,6 +1,6 @@
 //import {xc, PropDefMap, PropDef} from 'xtal-element/lib/XtalCore.js';
 import { html } from 'xtal-element/lib/html.js';
-//import('pass-prop/p-p-x.js');
+import('pass-prop/p-p.js');
 import('xtal-fetch/xtal-fetch-get.js');
 import('pass-down/p-d-x.js');
 import { define } from 'carbon-copy/c-c.js';
@@ -8,6 +8,7 @@ import('@alenaksu/json-viewer/build/index.js');
 import('aggregator-fn/ag-fn.js');
 import('ref-to/ref-to.js');
 import('xt-f/xt-f.js');
+import('ib-id/i-bid.js');
 const mainTemplate = html `
 <xtal-fetch-get fetch href={{href}}></xtal-fetch-get>
 <p-d-x vft=result val-filter="$.modules.[*].declarations[?(@.tagName=='{{tag}}')]" to=[-declarations] m=1></p-d-x>
@@ -16,16 +17,46 @@ const mainTemplate = html `
         return declarations[0];
     }
 </script></ag-fn>
-<p-d vft=value to=[-data] m=1></p-d>
+<p-d vft=value to=[-data] m=2></p-d>
 <mark></mark>
 <json-viewer -data></json-viewer>
-<main>
-  <ref-to a={{tag}}></ref-to>
-  <p-d vft=deref to=[-piped-chunk] m=1></p-d>
-  <xt-f -piped-chunk></xt-f> 
-  <p-d from=main to=details care-of=[-data] val=detail m=1></p-d>
-  <p-d from=main to=[-lhs] val=detail m=2></p-d> 
-</main>
+<p-p from-parent-or-host observe-prop=contractProp to=[-contract-prop] m=1></p-p>
+<ag-fn -data -contract-prop><script nomodule>
+    ({data, contractProp}) => {
+        console.log({data, contractProp});
+        if(data === undefined || data.members === undefined || contractProp === undefined) return;
+        const fields = data.members.filter(x=> x.kind ==='field' && !x.static);
+        const propVals = {};
+        for(const field of fields){
+            if(field.default !== undefined){
+                let val = field.default;
+                if(field.type !== undefined && field.type.text !== undefined){
+                    switch(field.type.text){
+                        case 'boolean':
+                            val = val === 'true';
+                            break;
+                        case 'object':
+                            val = eval('(' + val + ')'); //yikes
+                            break;
+                    }
+                }
+                propVals[field.name] = val;
+            } 
+
+        }
+        console.log({propVals});
+        const contract = fields.filter(x => x.name===contractProp && x.kind ==='field');
+        console.log(contract);
+        console.log(contract[0].default);
+    }
+</script></ag-fn>
+<ref-to a={{tag}}></ref-to>
+<p-d vft=deref to=[-piped-chunk] m=1></p-d>
+<xt-f -piped-chunk></xt-f>
+<i-bid -list><p-d></p-d></i-bid>
+<!-- <p-d from=main to=details care-of=[-data] val=detail m=1></p-d>
+<p-d from=main to=[-lhs] val=detail m=2></p-d>  -->
+
 
 <details>
     <summary>Event Details</summary>
