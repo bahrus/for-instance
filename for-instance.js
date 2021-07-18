132ea3258cfb673b4425869ae5248ca2347b8b17
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-17 22:56:04 -0400

    ...

--- for-instance.js
+++ for-instance.js
@@ -1,120 +1,127 @@
-import { XtalFetchViewElement, mergeProps, define } from 'xtal-element/XtalFetchViewElement.js';
-import { prependTag } from 'trans-render/prependTag.js';
-//const href = 'href';
-const tag = 'tag';
-const contract_prop = 'contract-prop';
-const skip_imports = 'skip-imports';
-/**
- * Test instances of custom element for custom event signature contracts.
- * @element for-instance
- */
-export class ForInstance extends XtalFetchViewElement {
-    constructor() {
-        super();
-        this.noShadow = true;
-        import('p-et-alia/p-d.js');
-        import('if-diff/if-diff-then-stiff.js');
-        import('@alenaksu/json-viewer/build/index.js');
+//import {xc, PropDefMap, PropDef} from 'xtal-element/lib/XtalCore.js';
+import { html } from 'xtal-element/lib/html.js';
+import('pass-prop/p-p.js');
+import('xtal-fetch/xtal-fetch-get.js');
+import('pass-down/p-d-x.js');
+import { define } from 'carbon-copy/c-c.js';
+import('@alenaksu/json-viewer/build/index.js');
+import('aggregator-fn/ag-fn.js');
+import('ref-to/ref-to.js');
+import('xt-f/xt-f.js');
+import('ib-id/i-bid.js');
+const mainTemplate = html `
+<xtal-fetch-get fetch href={{href}}></xtal-fetch-get>
+<p-d-x vft=result val-filter="$.modules.[*].declarations[?(@.tagName=='{{tag}}')]" to=[-declarations] m=1></p-d-x>
+<ag-fn -declarations><script nomodule>
+    ({declarations}) =>{
+        return declarations[0];
     }
-    get readyToInit() {
-        return super.readyToInit && this.tag !== undefined && this.contractProp !== undefined;
-    }
-    filterInitData(data) {
-        const esi = data;
-        const elementInfo = esi.tags?.find(tag => tag.name === this.tag);
-        if (elementInfo === undefined) {
-            // reject('No Element Info Found');
-            return { test: {}, elementInfo: {} };
-            //TODO
-        }
-        const test$ = elementInfo.properties.find(prop => prop.name === this.contractProp)?.default;
-        if (test$ === undefined) {
-            return { test: {}, elementInfo: {} };
-            //reject('No contract found');
-            //TODO
-        }
-        const test = JSON.parse(test$);
-        return { test, elementInfo };
-    }
-    get readyToRender() {
-        if (this.viewModel === undefined)
-            return false;
-        let trigger = this.viewModel.test.trigger;
-        if (trigger != undefined) {
-            const scr = document.createElement('script');
-            scr.type = 'module';
-            if (this.skipImports) {
-                const split = trigger.split('\n');
-                split.forEach((line, idx) => {
-                    if (line.trim().startsWith('import ')) {
-                        split[idx] = '//' + line;
+</script></ag-fn>
+<p-d vft=value to=[-data] m=2></p-d>
+<mark></mark>
+<json-viewer -data></json-viewer>
+<p-p from-parent-or-host observe-prop=contractProp to=[-contract-prop] m=1></p-p>
+<ag-fn -data -contract-prop><script nomodule>
+    ({data, contractProp}) => {
+        console.log({data, contractProp});
+        if(data === undefined || data.members === undefined || contractProp === undefined) return;
+        const fields = data.members.filter(x=> x.kind ==='field' && !x.static);
+        const propVals = {};
+        for(const field of fields){
+            if(field.default !== undefined){
+                let val = field.default;
+                if(field.type !== undefined && field.type.text !== undefined){
+                    switch(field.type.text){
+                        case 'boolean':
+                            val = val === 'true';
+                            break;
+                        case 'object':
+                            val = eval('(' + val + ')'); //yikes
+                            break;
                     }
-                });
-                trigger = split.join('\n');
-            }
-            scr.innerHTML = trigger;
-            document.head.appendChild(scr);
-        }
-        return true;
-    }
-    get mainTemplate() {
-        return mainTemplate;
-    }
-    get initTransform() {
-        return {
-            mark: this.tag + ', for instance.',
-            'json-viewer': [{ innerHTML: JSON.stringify(this.viewModel) }],
-            main: ({ target }) => {
-                const newElement = prependTag(target, this.tag, [, , { disabled: '2' }], {});
-                this.viewModel.elementInfo.properties.forEach(prop => {
-                    if (prop.default !== undefined) {
-                        switch (typeof prop.default) {
-                            case 'string':
-                                if (typeof (prop.type) === 'string') {
-                                    if (prop.type.indexOf('=>') > -1) {
-                                        return;
-                                    }
-                                }
-                                switch (prop.type) {
-                                    case 'string':
-                                    case 'object':
-                                    case undefined:
-                                        newElement[prop.name] = JSON.parse(prop.default);
-                                        break;
-                                    default:
-                                        if (prop.type[0] === '{') { //example:   "type": "{ [key: string]: number; }",
-                                            newElement[prop.name] = JSON.parse(prop.default);
-                                        }
-                                        else {
-                                            newElement[prop.name] = prop.default;
-                                        }
-                                }
-                                break;
-                            default:
-                                newElement[prop.name] = prop.default;
-                        }
-                    }
-                });
-            },
-            '"': {
-                'p-d': [{ on: this.viewModel.test.expectedEvent.name }],
-            },
-            details: {
-                'section[data-lhs]': {
-                    'json-viewer': [{ innerHTML: JSON.stringify(this.viewModel.test.expectedEvent.detail) }]
                 }
-            },
-            'if-diff-then-stiff': [{ rhs: this.viewModel.test.expectedEvent.detail }]
-        };
+                propVals[field.name] = val;
+            } 
+
+        }
+        const contracts = fields.filter(x => x.name===contractProp && x.kind ==='field');
+        if(contracts.length === 1){
+            const defaultVal = contracts[0]?.default;
+            console.log({defaultVal});
+            if(defaultVal !== undefined){
+                const defaultObj = eval('(' + defaultVal + ')');
+                console.log(defaultObj);
+                const trigger = defaultObj?.trigger;
+                console.log(trigger);
+                if(trigger !== undefined){
+                    const scr = document.createElement('script');
+                    scr.type = 'module';
+                    scr.innerHTML = trigger;
+                    document.head.appendChild(scr);
+                }
+            }
+        }
+        return propVals;
     }
-}
-ForInstance.is = 'for-instance';
-ForInstance.attributeProps = ({ skipImports, tag, contractProp }) => {
-    const ap = {
-        bool: [skipImports],
-        str: [tag, contractProp],
-        reflect: [skipImports, tag, contract_prop]
-    };
-    return mergeProps(ap, XtalFetchViewElement.props);
-};
-define(ForInstance);
+</script></ag-fn>
+<p-d vft=value to={{tag}} prop=...></p-d>
+<ref-to a={{tag}}></ref-to>
+<p-d vft=deref to=[-piped-chunk] m=1></p-d>
+<xt-f -piped-chunk></xt-f>
+<i-bid -list><p-d></p-d></i-bid>
+<!-- <p-d from=main to=details care-of=[-data] val=detail m=1></p-d>
+<p-d from=main to=[-lhs] val=detail m=2></p-d>  -->
+
+
+<details>
+    <summary>Event Details</summary>
+    <section data-lhs>
+        <h4>Expected Event Detail</h4>
+        <json-viewer></json-viewer>
+    </section>
+    <section>
+        <h4>Actual Event Detail</h4>
+        <json-viewer -data></json-viewer>
+    </section>
+</details>
+<if-diff-then-stiff if -lhs equals -rhs data-key-name=success></if-diff-then-stiff>
+<if-diff-then-stiff if -lhs not_equals -rhs data-key-name=failure></if-diff-then-stiff>
+<div data-success=0>
+    <template>
+        <div mark style="background-color: green; color: white;">selectedElementContract succeeded.</div>
+    </template>
+</div>
+<div data-failure=0>
+    <template>
+      <div err style="background-color: red; color: white;">selectedElementContract failed.</div>
+    </template>
+</div>
+`;
+define('for-instance', mainTemplate, {
+    stringProps: ['tag', 'href', 'contractProp'],
+    boolProps: ['skipImports']
+});
+// /** */
+// export class ForInstance extends HTMLElement{
+//     static is = 'for-instance';
+// }
+// const baseProp: PropDef = {
+//     dry: true,
+//     async: true,
+// }
+// const strProp1: PropDef = {
+//     ...baseProp,
+//     type: String,
+// }
+// const boolProp1: PropDef = {
+//     ...baseProp,
+//     type: Boolean,
+// }
+// const propDefs: PropDefMap<ForInstance> = {
+//     tag: strProp1,
+//     href: strProp1,
+//     contractProp: strProp1,
+//     skipImports: boolProp1,
+// };
+//xc.define(ForInstance);
+//export interface ForInstance extends ForInstanceProps{}
--- for-instance.ts
+++ for-instance.ts
@@ -1,152 +1,131 @@
-import { XtalFetchViewElement, AttributeProps, mergeProps, define, TransformValueOptions, PSettings, RenderContext } from 'xtal-element/XtalFetchViewElement.js';
-import { ElementInfo, ElementSetInfo } from 'api-viewer-element/src/lib/types.js';
-import { createTemplate } from 'trans-render/createTemplate.js';
-import { PDProps } from 'p-et-alia/types.d.js';
-import { IfDiffProps } from 'if-diff/types.d.js';
-import { ForInstanceViewModel, Test, ForInstanceProps } from './types.js';
-import { prependTag } from 'trans-render/prependTag.js';
+//import {xc, PropDefMap, PropDef} from 'xtal-element/lib/XtalCore.js';
+import {html} from 'xtal-element/lib/html.js';
+import('pass-prop/p-p.js');
+import('xtal-fetch/xtal-fetch-get.js');
+import('pass-down/p-d-x.js');
+import {define} from 'carbon-copy/c-c.js';
+import {CCProps} from 'carbon-copy/types.d.js';
+import('@alenaksu/json-viewer/build/index.js');
+import('aggregator-fn/ag-fn.js');
+import('ref-to/ref-to.js');
+import('xt-f/xt-f.js');
+import('ib-id/i-bid.js');
 
-
-
-//const href = 'href';
-const tag = 'tag';
-const contract_prop = 'contract-prop';
-const skip_imports = 'skip-imports';
-/**
- * Test instances of custom element for custom event signature contracts.
- * @element for-instance
- */
-export class ForInstance extends XtalFetchViewElement<ForInstanceViewModel>{
-  constructor() {
-    super();
-    import('p-et-alia/p-d.js');
-    import('if-diff/if-diff-then-stiff.js');
-    import('@alenaksu/json-viewer/build/index.js');
-  }
-  
-  static is = 'for-instance';
-
-
-
-  noShadow = true;
-
-
-
-  static attributeProps = ({skipImports, tag, contractProp}: ForInstance) =>{
-    const ap = {
-      bool: [skipImports],
-      str: [tag, contractProp],
-      reflect: [skipImports, tag, contract_prop]
-    } as AttributeProps;
-    return mergeProps(ap, XtalFetchViewElement.props) as AttributeProps;
-  }
-
-  get readyToInit() {
-    return super.readyToInit && this.tag !== undefined && this.contractProp !== undefined;
-  }
-
-  filterInitData(data: any){
-    const esi = data as ElementSetInfo;
-    const elementInfo = esi.tags?.find(tag => tag.name === this.tag);
-    if (elementInfo === undefined) {
-      
-      // reject('No Element Info Found');
-      return {test: {} as Test, elementInfo: {} as ElementInfo};
-      //TODO
+const mainTemplate = html`
+<xtal-fetch-get fetch href={{href}}></xtal-fetch-get>
+<p-d-x vft=result val-filter="$.modules.[*].declarations[?(@.tagName=='{{tag}}')]" to=[-declarations] m=1></p-d-x>
+<ag-fn -declarations><script nomodule>
+    ({declarations}) =>{
+        return declarations[0];
     }
-    const test$ = elementInfo.properties.find(prop => prop.name === this.contractProp)?.default as string;
-    if (test$ === undefined) {
-      return {test: {} as Test, elementInfo: {} as ElementInfo};
-      //reject('No contract found');
-      //TODO
-    }
-    const test = JSON.parse(test$) as Test;
-    return { test, elementInfo };
-  }
-
-
-  get readyToRender(){
-    if(this.viewModel === undefined) return false;
-    let trigger = this.viewModel.test.trigger;
-    if (trigger != undefined) {
-      const scr = document.createElement('script');
-      scr.type = 'module';
-      if (this.skipImports) {
-        const split = trigger.split('\n');
-        split.forEach((line, idx) => {
-          if (line.trim().startsWith('import ')) {
-            split[idx] = '//' + line;
-          }
-        });
-        trigger = split.join('\n');
-      }
-      scr.innerHTML = trigger;
-      document.head.appendChild(scr);
-    }
-    return true;
-  }
-
-  get mainTemplate() {
-    return mainTemplate;
-  }
-
-  get initTransform(){
-    return {
-      mark: this.tag! + ', for instance.',
-      'json-viewer': [{innerHTML: JSON.stringify(this.viewModel)}]  as PSettings<Partial<HTMLElement>>,
-      main: ({ target }: RenderContext<HTMLElement>) => {
-        const newElement = prependTag(target!, this.tag!, [,,{disabled:'2'}], {});
-        this.viewModel.elementInfo.properties.forEach(prop => {
-          if (prop.default !== undefined) {
-            switch (typeof prop.default) {
-              case 'string':
-                if(typeof(prop.type) === 'string'){
-                  if(prop.type.indexOf('=>') > -1){
-                    return;
-                  }
-                }
-                switch (prop.type) {
-                  case 'string':
-                  case 'object':
-                  case undefined:
-                    (<any>newElement)[prop.name] = JSON.parse(prop.default);
-                    break;
-                  default:
-                    if (prop.type[0] === '{') { //example:   "type": "{ [key: string]: number; }",
-                      (<any>newElement)[prop.name] = JSON.parse(prop.default);
-                    } else {
-                      (<any>newElement)[prop.name] = prop.default;
+</script></ag-fn>
+<p-d vft=value to=[-data] m=2></p-d>
+<mark></mark>
+<json-viewer -data></json-viewer>
+<p-p from-parent-or-host observe-prop=contractProp to=[-contract-prop] m=1></p-p>
+<ag-fn -data -contract-prop><script nomodule>
+    ({data, contractProp}) => {
+        console.log({data, contractProp});
+        if(data === undefined || data.members === undefined || contractProp === undefined) return;
+        const fields = data.members.filter(x=> x.kind ==='field' && !x.static);
+        const propVals = {};
+        for(const field of fields){
+            if(field.default !== undefined){
+                let val = field.default;
+                if(field.type !== undefined && field.type.text !== undefined){
+                    switch(field.type.text){
+                        case 'boolean':
+                            val = val === 'true';
+                            break;
+                        case 'object':
+                            val = eval('(' + val + ')'); //yikes
+                            break;
                     }
                 }
-                break;
-              default:
-                (<any>newElement)[prop.name] = prop.default;
-            }
+                propVals[field.name] = val;
+            } 
 
-          }
-
-        })
-      },
-      '"':{
-        'p-d':[{on: this.viewModel.test.expectedEvent.name}] as PSettings<PDProps>,
-      },
-      details: {
-        'section[data-lhs]': {
-          'json-viewer': [{innerHTML: JSON.stringify(this.viewModel.test.expectedEvent.detail)}]  as PSettings<Partial<HTMLElement>>
         }
-      },
-      'if-diff-then-stiff': [{rhs: this.viewModel.test.expectedEvent.detail}] as PSettings<IfDiffProps>
-    } as TransformValueOptions
-  }
+        const contracts = fields.filter(x => x.name===contractProp && x.kind ==='field');
+        if(contracts.length === 1){
+            const defaultVal = contracts[0]?.default;
+            console.log({defaultVal});
+            if(defaultVal !== undefined){
+                const defaultObj = eval('(' + defaultVal + ')');
+                console.log(defaultObj);
+                const trigger = defaultObj?.trigger;
+                console.log(trigger);
+                if(trigger !== undefined){
+                    const scr = document.createElement('script');
+                    scr.type = 'module';
+                    scr.innerHTML = trigger;
+                    document.head.appendChild(scr);
+                }
+            }
+        }
+        return propVals;
+    }
+</script></ag-fn>
+<p-d vft=value to={{tag}} prop=...></p-d>
+<ref-to a={{tag}}></ref-to>
+<p-d vft=deref to=[-piped-chunk] m=1></p-d>
+<xt-f -piped-chunk></xt-f>
+<i-bid -list><p-d></p-d></i-bid>
+<!-- <p-d from=main to=details care-of=[-data] val=detail m=1></p-d>
+<p-d from=main to=[-lhs] val=detail m=2></p-d>  -->
 
-}
 
-export interface ForInstance extends ForInstanceProps{}
+<details>
+    <summary>Event Details</summary>
+    <section data-lhs>
+        <h4>Expected Event Detail</h4>
+        <json-viewer></json-viewer>
+    </section>
+    <section>
+        <h4>Actual Event Detail</h4>
+        <json-viewer -data></json-viewer>
+    </section>
+</details>
+<if-diff-then-stiff if -lhs equals -rhs data-key-name=success></if-diff-then-stiff>
+<if-diff-then-stiff if -lhs not_equals -rhs data-key-name=failure></if-diff-then-stiff>
+<div data-success=0>
+    <template>
+        <div mark style="background-color: green; color: white;">selectedElementContract succeeded.</div>
+    </template>
+</div>
+<div data-failure=0>
+    <template>
+      <div err style="background-color: red; color: white;">selectedElementContract failed.</div>
+    </template>
+</div>
+`;
+define('for-instance', mainTemplate, {
+    stringProps: ['tag', 'href', 'contractProp'],
+    boolProps: ['skipImports']
+} as CCProps);
 
-define(ForInstance);
+// /** */
+// export class ForInstance extends HTMLElement{
+//     static is = 'for-instance';
 
-declare global {
-  interface HTMLElementTagNameMap {
-    "for-instance": ForInstance,
-  }
-}
+// }
+// const baseProp: PropDef = {
+//     dry: true,
+//     async: true,
+// }
+// const strProp1: PropDef = {
+//     ...baseProp,
+//     type: String,
+// }
+// const boolProp1: PropDef = {
+//     ...baseProp,
+//     type: Boolean,
+// }
+// const propDefs: PropDefMap<ForInstance> = {
+//     tag: strProp1,
+//     href: strProp1,
+//     contractProp: strProp1,
+//     skipImports: boolProp1,
+// };
+//xc.define(ForInstance);
+//export interface ForInstance extends ForInstanceProps{}
